6a778a52ec03568d9c2bbf441c293b96bca4aeda
Michael Zarif <michaelzsobhy@gmail.com>
2022-05-19 16:16:05 +0200

    Adding nested for loops

--- googleCrashCourse.py
+++ googleCrashCourse.py
@@ -1,1 +1,12 @@
-print("Hello Python!")
+# Domino Tiles
+for left in range(7):
+    for right in range(left, 7):
+        print(f"[{left}|{right}]", end=" ")
+    print()
+
+# Home-Away Tournament
+teams = ["Dragons", "Wolves", "Pandas", "Unicorns"]
+for home_team in teams:
+    for away_team in teams:
+        if home_team != away_team:
+            print(f"{home_team} vs {away_team}")
